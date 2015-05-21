c1baedcfd2399f529b1537ef37c1fa623948b2cd
pedro <pedrogas_g@hotmail.com>
2015-05-22 00:23:37 +0200

    For coursera programing in R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## A special matrix for coursera that have the memoization feuture of the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set,
+       get=get,
+       setinverse=setinverse,
+       getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Chache Resolver to check if the inver matrix is cached.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(is.null(inv)) {
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+  }else{
+    message("getting cached data.")
+  }
+  inv
 }
